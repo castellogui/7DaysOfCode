445d60f687fd781468ecf1847ae0b20f7bf357bc
Guilherme Castello <guilherme.brasolin@outlook.com>
2022-04-26 10:45:40 -0300

    Retirando imports não utilizados

--- src/TerceiroDia/DesafioEntidadeFilme.java
+++ src/TerceiroDia/DesafioEntidadeFilme.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("DuplicatedCode")
